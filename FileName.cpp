85bb190b16c26fd3fb5421ad8374b8b8c15fd892
egor4477 <k.egorishe4477@gmail.com>
2025-05-21 15:43:03 +0300

    Newcode11

--- FileName.cpp
+++ FileName.cpp
@@ -3,6 +3,29 @@
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "");
+
+    double Q, W, t, S, n, H, Hr, h, Hc, Re, L, d, V, g, ny, ld;
+
+    // Ввод данных
+    cout << "Введите t: ";
+    cin >> t;
+    cout << "Введите S: ";
+    cin >> S;
+    cout << "Введите n: ";
+    cin >> n;
+    cout << "Введите Hr: ";
+    cin >> Hr;
+    cout << "Введите L: ";
+    cin >> L;
+    cout << "Введите d: ";
+    cin >> d;
+
+    // Константы
+    g = 9.8;
+    V = 1.0; // Предполагаемая скорость потока, м/с
+    ny = 1e-6;
+    Hc = 5;
 
     // Расчеты
     W = 0.02 * S + 0.05 * n;
@@ -20,6 +43,9 @@ int main() {
     h = ld * (L / d) + (V * V) / (2 * g);
     H = Hr + h + Hc;
 
+    // Вывод
+    cout << "Вывод Q: " << Q << endl;
+    cout << "Вывод H: " << H << endl;
 
     return 0;
 }
