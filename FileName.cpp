a893e27134da91194b85bc7e72ab16d6a147baa0
egor4477 <k.egorishe4477@gmail.com>
2025-05-21 15:46:33 +0300

    try2

--- FileName.cpp
+++ FileName.cpp
@@ -27,21 +27,7 @@ int main() {
     ny = 1e-6;
     Hc = 5;
 
-    // Расчеты
-    W = 0.02 * S + 0.05 * n;
-    Q = W / t;
-
-    Re = (d * V) / ny;
-
-    if (Re < 2300) {
-        ld = 64 / Re;
-    }
-    else {
-        ld = 0.3 / pow(Re, 0.25);
-    }
-
-    h = ld * (L / d) + (V * V) / (2 * g);
-    H = Hr + h + Hc;
+    
 
     // Вывод
     cout << "Вывод Q: " << Q << endl;
